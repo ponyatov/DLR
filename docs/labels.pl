0efad41e0957ce4816cf244bd9c4fe929abce488
Dmitry Ponyatov <dponyatov@gmail.com>
2017-11-04 00:06:19 +0400

    v why language
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- docs/labels.pl
+++ docs/labels.pl
@@ -2,19 +2,15 @@
 # Associate labels original text with physical files.
 
 
+$key = q/cite_PLAI/;
+$external_labels{$key} = "$URL/" . q|node15.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/newbie/;
 $external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/implementing/;
-$external_labels{$key} = "$URL/" . q|node6.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/reference/;
-$external_labels{$key} = "$URL/" . q|node11.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_PLAI/;
+$key = q/cite_bluebook/;
 $external_labels{$key} = "$URL/" . q|node15.html|; 
 $noresave{$key} = "$nosave";
 
@@ -22,20 +18,24 @@ $key = q/cite_dragon/;
 $external_labels{$key} = "$URL/" . q|node15.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/forth/;
+$external_labels{$key} = "$URL/" . q|node14.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/whylanguage/;
 $external_labels{$key} = "$URL/" . q|node4.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/reference/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/implementing/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/terminology/;
 $external_labels{$key} = "$URL/" . q|node5.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_bluebook/;
-$external_labels{$key} = "$URL/" . q|node15.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/forth/;
-$external_labels{$key} = "$URL/" . q|node14.html|; 
 $noresave{$key} = "$nosave";
 
 1;
