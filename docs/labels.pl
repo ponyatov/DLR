b46a09ba3afea1d02c865af838d39e07c6125013
Dmitry Ponyatov <dponyatov@gmail.com>
2017-11-03 23:25:03 +0400

    * structure not using relative sectioning
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- docs/labels.pl
+++ docs/labels.pl
@@ -2,6 +2,42 @@
 # Associate labels original text with physical files.
 
 
+$key = q/newbie/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/implementing/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/reference/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_PLAI/;
+$external_labels{$key} = "$URL/" . q|node15.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_dragon/;
+$external_labels{$key} = "$URL/" . q|node15.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/whylanguage/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/terminology/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_bluebook/;
+$external_labels{$key} = "$URL/" . q|node15.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/forth/;
+$external_labels{$key} = "$URL/" . q|node14.html|; 
+$noresave{$key} = "$nosave";
+
 1;
 
 
