188c2418925e837e394730014a93c09cc2e1911c
Dmitry Ponyatov <dponyatov@gmail.com>
2017-11-06 15:10:20 +0400

    + registers
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- doc/py03.py
+++ doc/py03.py
@@ -1,18 +1,17 @@
 import sys
 
 class VM:
+    R = [0,1,2,3,4,5,6,7]    # registers shared between VM instances
     # command set
     def nop(self): pass             # do nothing
-    def bye(self): self._bye=True   # stop single VM only
-    # sequence interpreter
+    def bye(self): self._bye=True   # stop singe VM only
     def interpreter(self):
         while not self._bye:
             assert self.Ip < len(self.program)
             command = self.program[self.Ip]
-            print '%.4X' % self.Ip , command
+            print '%.4X' % self.Ip , command, self.R
             self.Ip += 1
             command(self)
-    # virtual machine constructor
     def __init__(self, P=[]):
         self.program = P        # load program
         self.Ip = 0             # set instruction pointer
