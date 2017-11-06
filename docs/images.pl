995fa7aabf85ce8758e8f0e18673b824fecb8c13
Dmitry Ponyatov <dponyatov@gmail.com>
2017-11-06 21:55:19 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- docs/images.pl
+++ docs/images.pl
@@ -2,58 +2,24 @@
 # Associate images original text with physical files.
 
 
-$key = q/{lstlisting}[language=Python]classVM:{par{defld(self):'loadregister'index=self.p__':VM([#everycommandneedVM.prefixVM.ld,1,'R[1]',VM.nop,VM.bye]){lstlisting};AAT/;
+$key = q/{lstlisting}[language=C]#defineMsz0x1000slash*bytes*slash#defineRsz0x100#defineDsz0x10{lstlisting};AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="632" HEIGHT="344" ALIGN="BOTTOM" BORDER="0"
+ WIDTH="334" HEIGHT="53" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="\begin{lstlisting}[language=C]
+...">|; 
+
+$key = q/{lstlisting}[language=Python]classVM:{par{defld(self):'loadregister'#getregisterommandneedVM.prefixVM.ld,1,'R[1]',#R[1]<-'string'VM.nop,VM.bye]){lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="431" ALIGN="BOTTOM" BORDER="0"
  SRC="|."$dir".q|img6.png"
  ALT="\begin{lstlisting}[language=Python]
 class VM:
 \par
 def ld(self):
 ' load registe...
-... command need VM. prefix
-VM.ld, 1, 'R[1]',
-VM.nop, VM.bye
-])
-\end{lstlisting}">|; 
-
-$key = q/{lstlisting}[language=C++]externuint32_tCp;slashslashcompilationpointer(freeheap){lstlisting};AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="567" HEIGHT="29" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img14.png"
- ALT="\begin{lstlisting}[language=C++]
-extern uint32_t Cp; // compilation pointer (free heap)
-\end{lstlisting}">|; 
-
-$key = q/{lstlisting}[language=C++]voidCbyte(uint8_tb){M[Cp++]=b;assert(Cp<Msz);}voidCcelashlengthmemcpy(&M[Cp],s,L+1);Cp+=L+1;}slashslashcompilelength+1{lstlisting};AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="567" HEIGHT="134" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img16.png"
- ALT="\begin{lstlisting}[language=C++]
-void Cbyte( uint8_t b) {
-M[Cp++] = b; assert(C...
-... length
-memcpy(&amp;M[Cp],s,L+1); Cp += L+1; } // compile length+1
-\end{lstlisting}">|; 
-
-$key = q/{lstlisting}[language=C++]voidset(uint32_taddr,int32_tvalue){assert(addr+3<Msz);0xFF;M[addr+2]=(value>>0x10)&0xFF;M[addr+3]=(value>>0x18)&0xFF;}{lstlisting};AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="567" HEIGHT="116" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img12.png"
- ALT="\begin{lstlisting}[language=C++]
-void set(uint32_t addr, int32_t value) {
-asser...
-...2] = (value&#187;0x10) &amp; 0xFF;
-M[addr+3] = (value&#187;0x18) &amp; 0xFF; }
-\end{lstlisting}">|; 
-
-$key = q/{lstlisting}[language=Python]classVM:R=[0,1,2,3,4,5,6,7]#registersshared#betweenVMinstances{lstlisting};AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="567" HEIGHT="64" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img5.png"
- ALT="\begin{lstlisting}[language=Python]
-class VM:
-R = [0,1,2,3,4,5,6,7]  ...">|; 
+...refix
+VM.ld, 1, 'R[1]',  ...">|; 
 
 $key = q/{lstlisting}[language=C]externvoidset(uint32_taddr,int32_tvalue);externuint32_tget(uint32_taddr);{lstlisting};AAT/;
 $cached_env_img{$key} = q|<IMG
@@ -63,6 +29,21 @@ $cached_env_img{$key} = q|<IMG
 extern void set(uint32_t addr, int32_t value);
 extern uint32_t get(uint32_t addr);
 \end{lstlisting}">|; 
+
+$key = q/{lstlisting}[language=C]#defineCELLsizeof(int32_t){lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="29" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="\begin{lstlisting}[language=C]
+...">|; 
+
+$key = q/{lstlisting}[language=Python]classVM:#programmemorysharedbetweenVMinstancesprogr=[0,1,2,3,4,5,6,7]#registerpoolifP:self.program=P#loadprogram...{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="151" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="\begin{lstlisting}[language=Python]
+class VM:
+...">|; 
 
 $key = q/{lstlisting}[language=C++]intmain(){...slashslashcompilevocabularyheaderyyparse(ilerdump();slashslashdumpmemoryinto.binfileVM();slashslashrunVM}{lstlisting};AAT/;
 $cached_env_img{$key} = q|<IMG
@@ -89,61 +70,6 @@ map&lt;string,uint32_t&gt; SymTable; // symbol table
 LFA(); AFA(); NFA(name); CFA(name); }
 \end{lstlisting}">|; 
 
-$key = q/{lstlisting}[language=C]externuint8_tM[Msz];slashslashmemoryexternuint32_tIp=0;spointerexternuint32_tCp=0;slashslashcompilationpointer(freeheap){lstlisting};AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="567" HEIGHT="64" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img10.png"
- ALT="\begin{lstlisting}[language=C]
-extern uint8_t M[Msz]; // memory
-extern uint32_t ...
-...ointer
-extern uint32_t Cp=0; // compilation pointer (free heap)
-\end{lstlisting}">|; 
-
-$key = q/{lstlisting}0000<functionnopat0x7ff7bb790b18>0001<functionbyeat0x7ff7bb790c08>{lstlisting};AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="567" HEIGHT="46" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img3.png"
- ALT="\begin{lstlisting}
-0000 &lt;function nop at 0x7ff7bb790b18&gt;
-0001 &lt;function bye at 0x7ff7bb790c08&gt;
-\end{lstlisting}">|; 
-
-$key = q/{lstlisting}[language=C++]externvoidCbyte(uint8_t);slashslashcompilebyteexternvoompilecellexternvoidCstring(char*);slashslashcompileASCIIZstring{lstlisting};AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="567" HEIGHT="64" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img15.png"
- ALT="\begin{lstlisting}[language=C++]
-extern void Cbyte( uint8_t); // compile byte
-ex...
-...mpile cell
-extern void Cstring(char*); // compile ASCIIZ string
-\end{lstlisting}">|; 
-
-$key = q/{lstlisting}[language=C++]#defineopNOP0x00slashslashnop{lstlisting};AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="567" HEIGHT="64" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img23.png"
- ALT="ALT=""">|; 
-
-$key = q/C^{++};MSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="37" HEIGHT="19" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img7.png"
- ALT="$C^{++}$">|; 
-
-$key = q/{lstlisting}[language=C++]slashslashprogramentrypoint(addrofjmpparameter)#definee(opJMP);Ccell(0);slashslash_latestLFAoflastdefinedwordCcell(0);{lstlisting};AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="567" HEIGHT="204" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img17.png"
- ALT="\begin{lstlisting}[language=C++]
-// program entry point (addr of jmp parameter)
-...
-...JMP); Ccell(0);
-// _latest LFA of last defined word
-Ccell(0);
-\end{lstlisting}">|; 
-
 $key = q/{lstlisting}[language=C++]voidVM(){for(;;){slashslashinftyloopprintf("<comment_mit();break;default:printf("badopcodenn");abort();}printf("n");}}{lstlisting};AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="567" HEIGHT="291" ALIGN="BOTTOM" BORDER="0"
@@ -158,6 +84,34 @@ printf(''\n'');
 }}
 \end{lstlisting}">|; 
 
+$key = q/{lstlisting}[language=C++]voidCbyte(uint8_tb){M[Cp++]=b;assert(Cp<Msz);}voidCcelashlengthmemcpy(&M[Cp],s,L+1);Cp+=L+1;}slashslashcompilelength+1{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="134" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img16.png"
+ ALT="\begin{lstlisting}[language=C++]
+void Cbyte( uint8_t b) {
+M[Cp++] = b; assert(C...
+... length
+memcpy(&amp;M[Cp],s,L+1); Cp += L+1; } // compile length+1
+\end{lstlisting}">|; 
+
+$key = q/C^{++};MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="37" HEIGHT="19" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$C^{++}$">|; 
+
+$key = q/{lstlisting}[language=C++]voidset(uint32_taddr,int32_tvalue){assert(addr+3<Msz);0xFF;M[addr+2]=(value>>0x10)&0xFF;M[addr+3]=(value>>0x18)&0xFF;}{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="116" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img12.png"
+ ALT="\begin{lstlisting}[language=C++]
+void set(uint32_t addr, int32_t value) {
+asser...
+...2] = (value&#187;0x10) &amp; 0xFF;
+M[addr+3] = (value&#187;0x18) &amp; 0xFF; }
+\end{lstlisting}">|; 
+
 $key = q/{lstlisting}0000<unboundmethodVM.nop>0001<unboundmethodVM.bye>{lstlisting};AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="567" HEIGHT="46" ALIGN="BOTTOM" BORDER="0"
@@ -165,6 +119,32 @@ $cached_env_img{$key} = q|<IMG
  ALT="\begin{lstlisting}
 0000 &lt;unbound method VM.nop&gt;
 0001 &lt;unbound method VM.bye&gt;
+\end{lstlisting}">|; 
+
+$key = q/{framed}Thekeyproperty:compileremph{compilesbutnotexecutesprogram}{framed};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="556" HEIGHT="44" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\begin{framed}
+The key property: compiler \emph{compiles but not executes program}
+\end{framed}">|; 
+
+$key = q/{framed}InterpreterisacomputerprogramwritteninlanguageLemph{executes}programwritteninlanguageP.{framed};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="556" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="\begin{framed}
+Interpreter is a computer program written in language L \emph{executes} program
+written in language P.
+\end{framed}">|; 
+
+$key = q/{lstlisting}0000<functionnopat0x7ff7bb790b18>0001<functionbyeat0x7ff7bb790c08>{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="46" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="\begin{lstlisting}
+0000 &lt;function nop at 0x7ff7bb790b18&gt;
+0001 &lt;function bye at 0x7ff7bb790c08&gt;
 \end{lstlisting}">|; 
 
 $key = q/{lstlisting}[language=C++]uint32_tget(uint32_taddr){assert(addr+3<Msz);returnM[addr+0]<<0x00|M[addr+1]<<0x08|M[addr+2]<<0x10|M[addr+3]<<0x18;}{lstlisting};AAT/;
@@ -178,36 +158,53 @@ assert(addr+3 &lt; ...
 M[addr+2]&#171;0x10 \vert M[addr+3]&#171;0x18; }
 \end{lstlisting}">|; 
 
-$key = q/{framed}Thekeyproperty:compileremph{compilesbutnotexecutesprogram}{framed};AAT/;
+$key = q/{lstlisting}[language=C++]externvoidCbyte(uint8_t);slashslashcompilebyteexternvoompilecellexternvoidCstring(char*);slashslashcompileASCIIZstring{lstlisting};AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="556" HEIGHT="44" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img1.png"
- ALT="\begin{framed}
-The key property: compiler \emph{compiles but not executes program}
-\end{framed}">|; 
+ WIDTH="567" HEIGHT="64" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img15.png"
+ ALT="\begin{lstlisting}[language=C++]
+extern void Cbyte( uint8_t); // compile byte
+ex...
+...mpile cell
+extern void Cstring(char*); // compile ASCIIZ string
+\end{lstlisting}">|; 
 
-$key = q/{lstlisting}[language=C]#defineCELLsizeof(int32_t){lstlisting};AAT/;
+$key = q/{lstlisting}[language=C++]slashslashprogramentrypoint(addrofjmpparameter)#definee(opJMP);Ccell(0);slashslash_latestLFAoflastdefinedwordCcell(0);{lstlisting};AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="567" HEIGHT="204" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img17.png"
+ ALT="\begin{lstlisting}[language=C++]
+// program entry point (addr of jmp parameter)
+...
+...JMP); Ccell(0);
+// _latest LFA of last defined word
+Ccell(0);
+\end{lstlisting}">|; 
+
+$key = q/{lstlisting}[language=C++]externuint32_tCp;slashslashcompilationpointer(freeheap){lstlisting};AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="567" HEIGHT="29" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img9.png"
- ALT="\begin{lstlisting}[language=C]
-...">|; 
+ SRC="|."$dir".q|img14.png"
+ ALT="\begin{lstlisting}[language=C++]
+extern uint32_t Cp; // compilation pointer (free heap)
+\end{lstlisting}">|; 
 
-$key = q/{lstlisting}[language=C]#defineMsz0x1000slash*bytes*slash#defineRsz0x100#defineDsz0x10{lstlisting};AAT/;
+$key = q/{lstlisting}[language=C++]#defineopNOP0x00slashslashnop{lstlisting};AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="334" HEIGHT="53" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img8.png"
- ALT="\begin{lstlisting}[language=C]
-...">|; 
+ WIDTH="567" HEIGHT="64" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img23.png"
+ ALT="ALT=""">|; 
 
-$key = q/{framed}InterpreterisacomputerprogramwritteninlanguageLemph{executes}programwritteninlanguageP.{framed};AAT/;
+$key = q/{lstlisting}[language=C]externuint8_tM[Msz];slashslashmemoryexternuint32_tIp=0;spointerexternuint32_tCp=0;slashslashcompilationpointer(freeheap){lstlisting};AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="556" HEIGHT="63" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img2.png"
- ALT="\begin{framed}
-Interpreter is a computer program written in language L \emph{executes} program
-written in language P.
-\end{framed}">|; 
+ WIDTH="567" HEIGHT="64" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="\begin{lstlisting}[language=C]
+extern uint8_t M[Msz]; // memory
+extern uint32_t ...
+...ointer
+extern uint32_t Cp=0; // compilation pointer (free heap)
+\end{lstlisting}">|; 
 
 1;
 
--- docs/labels.pl
+++ docs/labels.pl
@@ -2,36 +2,60 @@
 # Associate labels original text with physical files.
 
 
-$key = q/Fheap/;
-$external_labels{$key} = "$URL/" . q|node31.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_PLAI/;
+$key = q/cite_crafting/;
 $external_labels{$key} = "$URL/" . q|node57.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/terminology/;
-$external_labels{$key} = "$URL/" . q|node6.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/syntax/;
+$key = q/semantics/;
 $external_labels{$key} = "$URL/" . q|node9.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/forth/;
-$external_labels{$key} = "$URL/" . q|node27.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_ans94/;
-$external_labels{$key} = "$URL/" . q|node57.html|; 
+$key = q/dynacomp/;
+$external_labels{$key} = "$URL/" . q|node13.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/whylanguage/;
 $external_labels{$key} = "$URL/" . q|node5.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/semantics/;
+$key = q/newbie/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ans94/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/implementing/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Cxxx/;
+$external_labels{$key} = "$URL/" . q|node31.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/dynalang/;
+$external_labels{$key} = "$URL/" . q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_kim/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/syntax/;
 $external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_bluebook/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/forth/;
+$external_labels{$key} = "$URL/" . q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/vocabulary/;
+$external_labels{$key} = "$URL/" . q|node32.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/bytecode/;
@@ -42,52 +66,28 @@ $key = q/VM/;
 $external_labels{$key} = "$URL/" . q|node12.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/Cxxx/;
-$external_labels{$key} = "$URL/" . q|node31.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_bluebook/;
-$external_labels{$key} = "$URL/" . q|node57.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/dynacomp/;
-$external_labels{$key} = "$URL/" . q|node13.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_crafting/;
-$external_labels{$key} = "$URL/" . q|node57.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/vocabulary/;
-$external_labels{$key} = "$URL/" . q|node32.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_kim/;
-$external_labels{$key} = "$URL/" . q|node57.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/interpreter/;
 $external_labels{$key} = "$URL/" . q|node10.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/newbie/;
-$external_labels{$key} = "$URL/" . q|node4.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/reference/;
-$external_labels{$key} = "$URL/" . q|node19.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/dynalang/;
-$external_labels{$key} = "$URL/" . q|node13.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/implementing/;
-$external_labels{$key} = "$URL/" . q|node7.html|; 
+$key = q/cite_PLAI/;
+$external_labels{$key} = "$URL/" . q|node57.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_dragon/;
 $external_labels{$key} = "$URL/" . q|node57.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/terminology/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Fheap/;
+$external_labels{$key} = "$URL/" . q|node31.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/reference/;
+$external_labels{$key} = "$URL/" . q|node19.html|; 
 $noresave{$key} = "$nosave";
 
 1;
