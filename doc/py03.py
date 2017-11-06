a33c64dc0e79fe5d5d517316f993b7ebab6a8590
Dmitry Ponyatov <dponyatov@gmail.com>
2017-11-07 02:26:34 +0400

    * pyN.py
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- doc/py03.py
+++ doc/py03.py
@@ -1,36 +1,8 @@
-import sys
-
 class VM:
-    program = []                # program memory shared between VM instances
-    # command set
-    def nop(self): pass             # do nothing
-    def bye(self): self._bye=True   # stop singe VM only
-
-    def ld(self):
-        ' load register '
-        index = self.program[self.Ip]   # get register number
-        assert index < len(self.R)      # check index
-        self.Ip += 1                    # skip first command parameter
-        data = self.program[self.Ip]    # load data to be loaded
-        self.Ip += 1                    # skip second command parameter
-        self.R[index] = data            # load register
-         
-    def interpreter(self):
-        while not self._bye:
-            assert self.Ip < len(self.program)
-            command = self.program[self.Ip]
-            print '%.4X' % self.Ip , command, self.R
-            self.Ip += 1
-            command(self)
+	# program memory shared between VM instances
+    program = []
+    # constructor (re)loads program in P not empty  	
     def __init__(self, P=[]):
         self.R = [0,1,2,3,4,5,6,7]  # register pool
-        if P: self.program = P      # load program
-        self.Ip = 0                 # set instruction pointer
-        self._bye = False           # stop interpreter flag
-        self.interpreter()          # run interpreter
-
-if __name__ == '__main__':
-    VM([                      # every command need VM. prefix
-        VM.ld, 1, 'R[1]',   # R[1] <- 'string'
-        VM.nop, VM.bye
-    ])
+        if P: self.program = P		# load program
+        ...    
