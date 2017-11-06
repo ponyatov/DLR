3840f2d765da33077e089c6332c9a1f09b0030c1
Dmitry Ponyatov <dponyatov@gmail.com>
2017-11-06 21:54:54 +0400

    * static program[]
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- doc/py03.py
+++ doc/py03.py
@@ -1,7 +1,7 @@
 import sys
 
 class VM:
-    R = [0,1,2,3,4,5,6,7]    # registers shared between VM instances
+    program = []                # program memory shared between VM instances
     # command set
     def nop(self): pass             # do nothing
     def bye(self): self._bye=True   # stop singe VM only
@@ -9,6 +9,7 @@ class VM:
     def ld(self):
         ' load register '
         index = self.program[self.Ip]   # get register number
+        assert index < len(self.R)      # check index
         self.Ip += 1                    # skip first command parameter
         data = self.program[self.Ip]    # load data to be loaded
         self.Ip += 1                    # skip second command parameter
@@ -22,13 +23,14 @@ class VM:
             self.Ip += 1
             command(self)
     def __init__(self, P=[]):
-        self.program = P        # load program
-        self.Ip = 0             # set instruction pointer
-        self._bye = False       # stop interpreter flag
-        self.interpreter()      # run interpreter
+        self.R = [0,1,2,3,4,5,6,7]  # register pool
+        if P: self.program = P      # load program
+        self.Ip = 0                 # set instruction pointer
+        self._bye = False           # stop interpreter flag
+        self.interpreter()          # run interpreter
 
 if __name__ == '__main__':
     VM([                      # every command need VM. prefix
-        VM.ld, 1, 'R[1]',
+        VM.ld, 1, 'R[1]',   # R[1] <- 'string'
         VM.nop, VM.bye
     ])
