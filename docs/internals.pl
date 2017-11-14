ea215e038972c24e910c0bcf7e46b152c18cfac5
Dmitry Ponyatov <dponyatov@gmail.com>
2017-11-15 00:01:14 +0400

    html rel.141117

--- docs/internals.pl
+++ docs/internals.pl
@@ -2,96 +2,96 @@
 # Associate internals original text with physical files.
 
 
-$key = q/reference/;
-$ref_files{$key} = "$dir".q|node24.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/Cxxx/;
-$ref_files{$key} = "$dir".q|node36.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/whylanguage/;
-$ref_files{$key} = "$dir".q|node5.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/newbie/;
-$ref_files{$key} = "$dir".q|node4.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/bytecode/;
-$ref_files{$key} = "$dir".q|node11.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_dragon/;
-$ref_files{$key} = "$dir".q|node62.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/Fheap/;
-$ref_files{$key} = "$dir".q|node36.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/syntax/;
-$ref_files{$key} = "$dir".q|node9.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/interpreter/;
-$ref_files{$key} = "$dir".q|node10.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/SSA/;
-$ref_files{$key} = "$dir".q|node23.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/dynalang/;
 $ref_files{$key} = "$dir".q|node13.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_crafting/;
-$ref_files{$key} = "$dir".q|node62.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/dynacomp/;
-$ref_files{$key} = "$dir".q|node13.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_kim/;
-$ref_files{$key} = "$dir".q|node62.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/vocabulary/;
-$ref_files{$key} = "$dir".q|node37.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_ans94/;
-$ref_files{$key} = "$dir".q|node62.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_PLAI/;
-$ref_files{$key} = "$dir".q|node62.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/semantics/;
-$ref_files{$key} = "$dir".q|node9.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/forth/;
-$ref_files{$key} = "$dir".q|node32.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/implementing/;
-$ref_files{$key} = "$dir".q|node7.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/terminology/;
-$ref_files{$key} = "$dir".q|node6.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/VM/;
 $ref_files{$key} = "$dir".q|node12.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/cite_kim/;
+$ref_files{$key} = "$dir".q|node65.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/newbie/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ans94/;
+$ref_files{$key} = "$dir".q|node65.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_crafting/;
+$ref_files{$key} = "$dir".q|node65.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/forth/;
+$ref_files{$key} = "$dir".q|node35.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/reference/;
+$ref_files{$key} = "$dir".q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Cxxx/;
+$ref_files{$key} = "$dir".q|node39.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_dragon/;
+$ref_files{$key} = "$dir".q|node65.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Fheap/;
+$ref_files{$key} = "$dir".q|node39.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/syntax/;
+$ref_files{$key} = "$dir".q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/implementing/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/whylanguage/;
+$ref_files{$key} = "$dir".q|node5.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/terminology/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/SSA/;
+$ref_files{$key} = "$dir".q|node26.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/cite_bluebook/;
-$ref_files{$key} = "$dir".q|node62.html|; 
+$ref_files{$key} = "$dir".q|node65.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/interpreter/;
+$ref_files{$key} = "$dir".q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/bytecode/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/semantics/;
+$ref_files{$key} = "$dir".q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/vocabulary/;
+$ref_files{$key} = "$dir".q|node40.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_PLAI/;
+$ref_files{$key} = "$dir".q|node65.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/dynacomp/;
+$ref_files{$key} = "$dir".q|node13.html|; 
 $noresave{$key} = "$nosave";
 
 1;
--- docs/labels.pl
+++ docs/labels.pl
@@ -2,87 +2,75 @@
 # Associate labels original text with physical files.
 
 
-$key = q/newbie/;
-$external_labels{$key} = "$URL/" . q|node4.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/bytecode/;
-$external_labels{$key} = "$URL/" . q|node11.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/Cxxx/;
-$external_labels{$key} = "$URL/" . q|node36.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/reference/;
-$external_labels{$key} = "$URL/" . q|node24.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/whylanguage/;
-$external_labels{$key} = "$URL/" . q|node5.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/syntax/;
-$external_labels{$key} = "$URL/" . q|node9.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/interpreter/;
-$external_labels{$key} = "$URL/" . q|node10.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/SSA/;
-$external_labels{$key} = "$URL/" . q|node23.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cite_dragon/;
-$external_labels{$key} = "$URL/" . q|node62.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/Fheap/;
-$external_labels{$key} = "$URL/" . q|node36.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/dynacomp/;
-$external_labels{$key} = "$URL/" . q|node13.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/cite_kim/;
-$external_labels{$key} = "$URL/" . q|node62.html|; 
+$external_labels{$key} = "$URL/" . q|node65.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/dynalang/;
 $external_labels{$key} = "$URL/" . q|node13.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/VM/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/cite_crafting/;
-$external_labels{$key} = "$URL/" . q|node62.html|; 
+$external_labels{$key} = "$URL/" . q|node65.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/forth/;
+$external_labels{$key} = "$URL/" . q|node35.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/newbie/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ans94/;
+$external_labels{$key} = "$URL/" . q|node65.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/whylanguage/;
+$external_labels{$key} = "$URL/" . q|node5.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/terminology/;
 $external_labels{$key} = "$URL/" . q|node6.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/VM/;
-$external_labels{$key} = "$URL/" . q|node12.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/cite_bluebook/;
-$external_labels{$key} = "$URL/" . q|node62.html|; 
+$external_labels{$key} = "$URL/" . q|node65.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/vocabulary/;
-$external_labels{$key} = "$URL/" . q|node37.html|; 
+$key = q/SSA/;
+$external_labels{$key} = "$URL/" . q|node26.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_ans94/;
-$external_labels{$key} = "$URL/" . q|node62.html|; 
+$key = q/interpreter/;
+$external_labels{$key} = "$URL/" . q|node10.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cite_PLAI/;
-$external_labels{$key} = "$URL/" . q|node62.html|; 
+$key = q/bytecode/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/semantics/;
+$key = q/reference/;
+$external_labels{$key} = "$URL/" . q|node27.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_dragon/;
+$external_labels{$key} = "$URL/" . q|node65.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Cxxx/;
+$external_labels{$key} = "$URL/" . q|node39.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Fheap/;
+$external_labels{$key} = "$URL/" . q|node39.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/syntax/;
 $external_labels{$key} = "$URL/" . q|node9.html|; 
 $noresave{$key} = "$nosave";
 
@@ -90,8 +78,20 @@ $key = q/implementing/;
 $external_labels{$key} = "$URL/" . q|node7.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/forth/;
-$external_labels{$key} = "$URL/" . q|node32.html|; 
+$key = q/vocabulary/;
+$external_labels{$key} = "$URL/" . q|node40.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_PLAI/;
+$external_labels{$key} = "$URL/" . q|node65.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/dynacomp/;
+$external_labels{$key} = "$URL/" . q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/semantics/;
+$external_labels{$key} = "$URL/" . q|node9.html|; 
 $noresave{$key} = "$nosave";
 
 1;
