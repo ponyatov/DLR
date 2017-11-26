d32b433cf1419b3ceef41cdac4dbe94d735058b6
Dmitry Ponyatov <dponyatov@gmail.com>
2017-11-26 22:08:25 +0400

    + tex2html
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ tex2html.py
@@ -0,0 +1,8 @@
+# latex2html dows not works for me,
+# this is homebrew "crutches and bicycles" converter
+
+import os,sys
+
+html = open(sys.argv[0]+'.html','w')
+
+# process doc/Makefile to get file tree
