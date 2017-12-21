e9fee608f9ea5f7ee93948c0e5a7c4d3f250dc81
Dmitry Ponyatov <dponyatov@gmail.com>
2017-12-21 20:53:40 +0400

    + unification
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- yield.py
+++ yield.py
@@ -1,21 +1,33 @@
 # http://yieldprolog.sourceforge.net/tutorial1.html
 
-class Pvar:
-    def __init__(self):
-        self.bound = False      # unbound variable
-    def __lshift__(self,val):   # assign operator <<
-        self.bound = True
-        self.value = val
-    def __repr__(self):         # dump in string form
-        if self.bound:
-            return '<bound:%s>' % self.value
-        else:
-            return '<unbound>'
+class Pvar:						# Prolog unifying variable
+	def __init__(self):
+		self.bound = False      # unbound variable
+	def __lshift__(self,val):   # unify operator <<
+		if not self.bound:		# 1) unassigned
+			self.value = val	# assign var
+			self.bound = True
+			yield
+			self.bound = False	# drop binding
+		elif self.value == val:	# 2) assigned == val
+			yield
+	def __repr__(self):         # dump in string form
+		if self.bound:
+			return '<bound:%s>' % self.value
+		else:
+			return '<unbound>'
 
 def person(V):
-    V << 'Chelsea' ; yield
-    V << 'Hillary' ; yield
-    V << 'Bill'    ; yield
+	for i in V << 'Chelsea'	: yield
+	for i in V << 'Hillary'	: yield
+	for i in V << 'Bill'	: yield
 
 var = Pvar() ; print var
-for p in person(var): print var
+#for p in person(var): print var
+for i in var << 'Hillary':
+	for j in person(var):
+		print var
+for i in var << 'Buddy':
+	for j in person(var):
+		print var
+
