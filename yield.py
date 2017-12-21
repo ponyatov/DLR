9ddf405c7a4ec0b4c9c1bce68d2c13ffe7438a44
Dmitry Ponyatov <dponyatov@gmail.com>
2017-12-21 12:47:32 +0400

    + bound var
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- yield.py
+++ yield.py
@@ -1,11 +1,21 @@
 # http://yieldprolog.sourceforge.net/tutorial1.html
 
-class Pvar: pass    # empty container class
+class Pvar:
+    def __init__(self):
+        self.bound = False      # unbound variable
+    def __lshift__(self,val):   # assign operator <<
+        self.bound = True
+        self.value = val
+    def __repr__(self):         # dump in string form
+        if self.bound:
+            return '<bound:%s>' % self.value
+        else:
+            return '<unbound>'
 
 def person(V):
-    V.value = 'Chelsea' ; yield
-    V.value = 'Hillary' ; yield
-    V.value = 'Bill' ; yield
+    V << 'Chelsea' ; yield
+    V << 'Hillary' ; yield
+    V << 'Bill'    ; yield
 
-var = Pvar()
-for p in person(var): print var.value
+var = Pvar() ; print var
+for p in person(var): print var
