b95f0f525d2417f2773467efd3916c4a5d4c11da
Dmitry Ponyatov <dponyatov@gmail.com>
2017-12-22 07:36:15 +0400

    + chains unify
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- yield.py
+++ yield.py
@@ -1,38 +1,60 @@
 # http://yieldprolog.sourceforge.net/tutorial1.html
 
 class var:						# Prolog unifying variable
-	def __init__(self):
+	def __init__(self,name):
 		self.bound = False      # unbound variable
+		self.name = name
 	def __lshift__(self,val):   # unify operator <<
 		if not self.bound:		# 1) unassigned
-			self.value = val	# assign var
-			self.bound = True
-			yield
-			self.bound = False	# drop binding
-		elif self.value == val:	# 2) assigned == val
-			yield
+			self.value = getval(val) # get var
+			if self.value == self: yield # can be itself
+			else:
+				self.bound = True	# bound
+				yield
+				self.bound = False	# drop binding
+		else:
+			for i in unify(self,val): yield
 	def __repr__(self):         # dump in string form
 		if self.bound:
-			return '<bound:%s>' % self.value
+			return '%s=%s' % (self.name,self.value)
 		else:
-			return '<unbound>'
+			return '%s' % self.name
+	def getval(self):
+		if not self.bound:		# return unbound as is
+			return self
+		V = self.value				# else do loop until
+		while isinstance(V, var):	# non-variable found
+			if not V.bound: return V
+ 			V = V.value				# scan bound vars
+		return V		
 
 def getval(V):				# return value or unbound var
-	if isinstance(V,var):
-		if not V.bound: return V
-		else: return V.value
+	if isinstance(V,var): return V.getval()
 	else: return V
 
 def unify(arg1,arg2):			# general unification
 	arg1value = getval(arg1)		# \ get values
 	arg2value = getval(arg2)		# /
-	if isinstance(arg1value,var):	# unbound arg1
-		for i in arg1value << arg2value: yield
-	if isinstance(arg2value,var):	# unbound arg2
-		for j in arg2value << arg1value: yield
-	else:							# args non var's
+	A = B = False
+	if isinstance(arg1value,var):	# arg1 is variable
+		A = True
+ 		for i in arg1value << arg2value: yield
+	if isinstance(arg2value,var):	# arg2 is variable
+		B = True
+ 		for j in arg2value << arg1value: yield
+	if not (A and B):				# args non var's
 		if arg1value == arg2value: yield
 
+def square(Width,Height): # square rectangle
+	for i in unify(Width,Height): yield
+
+W,H = var('W'),var('H')
+for a in square(W,H):	print W,H
+for a in square(W,H):			# bind first
+   	for b in unify(W,33):		# unify with value
+ 		print W,H
+ 		
+##############################################################################
 def person(V):
 	for i in unify(V,'Chelsea'): yield
 	for i in unify(V,'Hillary'): yield
@@ -55,18 +77,3 @@ def uncle(Person,Uncle):
 	Parent = var()
 	for i in parent(Person,Parent):
 		for j in brother(Parent,Uncle): yield
-
-def square(Width,Height): # square rectangle
-	for i in unify(Width,Height): yield
-
-for i in square(11,11):	# check 11 x 11
-	print '11 x 11 is square'
-W,H = var(),var()
-for j in W << 22:
-	for k in square(W,H):
-		print W,H		# yields 22 x 22
-W,H = var(),var()
-for a in square(W,H):		# bind first
-	for b in unify(W,33):		# unify with value
-		print W,H
-
