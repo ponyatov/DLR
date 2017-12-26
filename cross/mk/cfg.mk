33df4035a4dbc2f420ce58117bb539f9c459c9b0
Dmitry Ponyatov <dponyatov@gmail.com>
2017-12-26 19:11:20 +0400

    + gmp
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cross/mk/cfg.mk
+++ cross/mk/cfg.mk
@@ -1,4 +1,3 @@
-CFG = configure --disable-nls \
-	--prefix=$(CROSS) --target=$(TARGET) \
+CFG = configure --disable-nls --prefix=$(CROSS) \
 	--docdir=$(TMP)/doc --mandir=$(TMP)/man --infodir=$(TMP)/info
 	
--- cross/mk/cross.mk
+++ cross/mk/cross.mk
@@ -6,17 +6,32 @@ BUILD_CPU_NUMBER = $(shell grep processor /proc/cpuinfo | wc -l)
 # parallel make
 MAKE = make -j$(BUILD_CPU_NUMBER)
 
+CROSS_CFG = --target=$(TARGET) 
+
 .PHONY: binutils
 binutils: $(CROSS)/bin/$(TARGET)-as
 $(CROSS)/bin/$(TARGET)-as: $(SRC)/$(BINUTILS)/README
 	rm -rf $(BLD)/$(BINUTILS) ; mkdir $(BLD)/$(BINUTILS) ;\
 	cd $(BLD)/$(BINUTILS) ; $(SRC)/$(BINUTILS)/$(CFG) \
-		--with-sysroot=$(ROOT) &&\
+		$(CROSS_CFG) --with-sysroot=$(ROOT) &&\
 	$(MAKE) && make install-strip
 
 .PHONY: gcc
 gcc: $(CROSS)/bin/$(TARGET)-gcc
 $(CROSS)/bin/$(TARGET)-gcc: $(SRC)/$(GCC)/README
 	rm -rf $(BLD)/$(GCC) ; mkdir $(BLD)/$(GCC) ;\
-	cd $(BLD)/$(GCC) ; $(SRC)/$(GCC)/$(CFG) \
+	cd $(BLD)/$(GCC) ; $(SRC)/$(GCC)/$(CFG) $(CROSS_CFG) \
 		--with-gmp=$(CROSS) --with-mpfr=$(CROSS) --with-mpc=$(CROSS)
+
+.PHONY: gcclibs
+gcclibs: gmp
+
+CCLIBS_CFG = --disable-shared
+
+.PHONY: gmp
+gmp: $(CROSS)/lib/libgmp.a
+$(CROSS)/lib/libgmp.a: $(SRC)/$(GMP)/README
+	rm -rf $(BLD)/$(GMP) ; mkdir $(BLD)/$(GMP) ;\
+	cd $(BLD)/$(GMP) ; $(SRC)/$(GMP)/$(CFG) $(CCLIBS_CFG) &&\
+	$(MAKE) && make install-strip
+ 
