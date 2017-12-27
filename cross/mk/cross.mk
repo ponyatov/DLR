efaa318851dcdfdcffaf20ecdc7c794a74811cc9
Dmitry Ponyatov <dponyatov@gmail.com>
2017-12-27 10:59:45 +0400

    + gcc0
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cross/mk/cross.mk
+++ cross/mk/cross.mk
@@ -1,28 +1,30 @@
 .PHONY: cross
-cross: binutils
+cross: binutils cclibs gcc0
 
 # get number or CPU cores for build system
 BUILD_CPU_NUMBER = $(shell grep processor /proc/cpuinfo | wc -l)
 # parallel make
 MAKE = make -j$(BUILD_CPU_NUMBER)
 
-CROSS_CFG = --target=$(TARGET)
 WITH_CCLIBS = --with-gmp=$(CROSS) --with-mpfr=$(CROSS) --with-mpc=$(CROSS)  
+CROSS_CFG = --target=$(TARGET) $(WITH_CCLIBS) \
+	--with-sysroot=$(ROOT) \
+	--with-native-system-header-dir=/include  
 
 .PHONY: binutils
 binutils: $(CROSS)/bin/$(TARGET)-as
 $(CROSS)/bin/$(TARGET)-as: $(SRC)/$(BINUTILS)/README
 	rm -rf $(BLD)/$(BINUTILS) ; mkdir $(BLD)/$(BINUTILS) ;\
-	cd $(BLD)/$(BINUTILS) ; $(SRC)/$(BINUTILS)/$(CFG) \
-		$(CROSS_CFG) --with-sysroot=$(ROOT) &&\
+	cd $(BLD)/$(BINUTILS) ; $(SRC)/$(BINUTILS)/$(CFG) $(CROSS_CFG) &&\
 	$(MAKE) && make install-strip
 
-.PHONY: gcc
-gcc: $(CROSS)/bin/$(TARGET)-gcc
-$(CROSS)/bin/$(TARGET)-gcc: $(SRC)/$(GCC)/README
+GCC0_CFG = --without-headers --with-newlib --enable-languages="c"
+.PHONY: gcc0
+gcc0: $(SRC)/$(GCC)/README
 	rm -rf $(BLD)/$(GCC) ; mkdir $(BLD)/$(GCC) ;\
 	cd $(BLD)/$(GCC) ; $(SRC)/$(GCC)/$(CFG) \
-		$(CROSS_CFG) $(WITH_CCLIBS)
+		$(CROSS_CFG) $(GCC0_CFG) &&\
+	$(MAKE) all-gcc && make install-gcc
 
 .PHONY: gcclibs
 gcclibs: gmp mpfr mpc
