fcfc8d66933ac17d5d781ded7a3ce15a67e8c927
Dmitry Ponyatov <dponyatov@gmail.com>
2017-12-28 10:28:35 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cross/mk/gz.mk
+++ cross/mk/gz.mk
@@ -3,7 +3,7 @@ PHONY: gz gz_cross gz_core
 gz: gz_cross gz_core
 
 gz_cross:
-	$(WGET) http://gcc.skazkaforyou.com/releases/$(GCC)/$(GCC_GZ)
+	$(WGET) http://ftp.gnu.org/gnu/binutils/$(BINUTILS_GZ)
 	exit -1
 	$(WGET) ftp://ftp.gmplib.org/pub/gmp/$(GMP_GZ)
 	$(WGET) http://www.mpfr.org/mpfr-$(MPFR_VER)/$(MPFR).tar.bz2
--- cross/mk/version.mk
+++ cross/mk/version.mk
@@ -2,7 +2,8 @@ GMP_VER = 6.1.2
 MPFR_VER = 3.1.6
 # MPFR 4.0.0 mpc build error: conflicting types for ‘mpfr_fmma’  
 MPC_VER = 1.0.3
-BINUTILS_VER = 2.29.1
+BINUTILS_VER = 2.24
+#2.29.1
 GCC_VER = 4.9.4
 #6.4.0
 #7.2.0
