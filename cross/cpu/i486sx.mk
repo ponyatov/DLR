db1d13cf199814e80b281498f7e0986f8450a6eb
Dmitry Ponyatov <dponyatov@gmail.com>
2017-12-28 07:28:26 +0400

    + kernel/qemu
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cross/cpu/i486sx.mk
+++ cross/cpu/i486sx.mk
@@ -1,3 +1,4 @@
 ARCH = i386
 # override CPU (will be used for TARGET)
 CPU = i486
+QEMU_CPU = 486
--- cross/mk/emu.mk
+++ cross/mk/emu.mk
@@ -1,4 +1,4 @@
-QEMU_CFG = -m 512M -net none -localtime 
+QEMU_CFG = -cpu $(QEMU_CPU) -m 64M -net none -localtime 
 QEMU_SERIAL = -serial file:ttyS0.log -append "console=ttyS0,115200" 
 #QEMU_CFG += $(QEMU_SERIAL)
 .PHONY: emu
