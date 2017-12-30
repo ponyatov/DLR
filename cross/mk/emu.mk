cb9c35cf12e4a61a176cfe5bbbc4b85ea849fbbd
Dmitry Ponyatov <dponyatov@gmail.com>
2017-12-30 21:05:43 +0400

    + root initrd boot
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cross/mk/emu.mk
+++ cross/mk/emu.mk
@@ -2,6 +2,15 @@ QEMU_CFG = -cpu $(QEMU_CPU) -m 512M -net none -localtime
 QEMU_SERIAL = -serial file:ttyS0.log -append "console=ttyS0,115200" 
 #QEMU_CFG += $(QEMU_SERIAL)
 .PHONY: emu
-emu: $(BOOT)/$(HW)_$(APP).kernel
-	qemu-system-$(ARCH) $(QEMU_CFG) -kernel $<
+emu: $(BOOT)/$(HW)_$(APP).kernel $(BOOT)/$(HW)_$(APP).rootfs
+	qemu-system-$(ARCH) $(QEMU_CFG) \
+		-kernel $(BOOT)/$(HW)_$(APP).kernel \
+		-initrd $(BOOT)/$(HW)_$(APP).rootfs
 #	 -append "noquiet vga=current loglevel=11"
+
+ROOTREX = "./(boot|include|lib/.+\.a)"
+.PHONY: root
+root: 
+	ln -fs /sbin/init $(ROOT)/init
+	cd $(ROOT) && find . | egrep -v $(ROOTREX) | cpio -o -H newc > $(BOOT)/$(HW)_$(APP).cpio
+	cat $(BOOT)/$(HW)_$(APP).cpio | gzip -9 > $(BOOT)/$(HW)_$(APP).rootfs
