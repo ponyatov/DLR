b7fb3fc6bba1012b241ff3b4f8be56945d77766d
Dmitry Ponyatov <dponyatov@gmail.com>
2018-01-06 14:07:07 +0400

    + int/hex/bin
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- SYM.py
+++ SYM.py
@@ -29,3 +29,33 @@ class Number(Primitive):
 
 def test_Number():
     assert re.match(r'num:1.2 #[0-9a-f]+',str(Number('1.2')))
+
+###
+
+class Integer(Number):
+	tag = 'int'
+	def __init__(self,V): self.val = int(V)
+
+print Integer('123')
+
+class Hex(Integer):
+	tag = 'hex'
+	def __init__(self,V): self.val = int(V,0x10)
+	def __repr__(self): return '%s:0x%X #%x' % (self.tag, self.val, id(self))
+
+print Hex('0xFF')
+
+class Binary(Integer):
+	tag = 'bin'
+	def __init__(self,V): self.val = int(V,0x02)
+	def __repr__(self): return '%s:%s #%x' % (self.tag, "{0:b}".format(self.val), id(self))
+
+print Binary('1111')
+
+class Active(Object): tag = 'act'
+
+class Operator(Active):
+	tag = 'op'
+
+print Operator('+')
+
