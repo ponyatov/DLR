db8bb7aeadbddfc48bc929de19fc129e624d037c
Dmitry Ponyatov <dponyatov@gmail.com>
2018-01-07 20:17:15 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- SYMGUI.py
+++ SYMGUI.py
@@ -11,8 +11,9 @@ class MainWindow(wx.Frame):    # inherit GUI widget
         # align on screen
         SW,SH = wx.GetDisplaySize()
         H = SH/2 ; W = H/4*3
+        RightCorner = (SW-W,SH-H) ; Center = (SW/5,SH/4)
         # initialize superclass
-        wx.Frame.__init__(self,None,title='SYM',pos=(SW-W,SH-H),size=(W,H))
+        wx.Frame.__init__(self,None,title='SYM',pos=Center,size=(W,H))
         # set window icon
         self.SetIcon(wx.ArtProvider.GetIcon(wx.ART_INFORMATION))
         # show window
@@ -20,18 +21,20 @@ class MainWindow(wx.Frame):    # inherit GUI widget
         # console font
         self.font = wx.Font(H/32,wx.FONTFAMILY_MODERN,wx.FONTSTYLE_NORMAL,wx.FONTWEIGHT_BOLD)
         # sizer
-        self.sizer = wx.BoxSizer(wx.VERTICAL) ; self.SetSizer(self.sizer)
-        # console
-        self.console = wx.TextCtrl(self,style=wx.TE_MULTILINE)
-        self.console.SetFont(self.font)
-        self.console.SetValue('# console')
-        # cmdline
-        self.cmdline = wx.TextCtrl(self,style=wx.TE_MULTILINE)
-        self.cmdline.SetFont(self.font)
-        self.cmdline.SetValue('# cmdline')
+        self.Hsizer = wx.BoxSizer(wx.VERTICAL)
+        self.Vsizer = wx.BoxSizer(wx.VERTICAL) 
+        self.SetSizer(self.Vsizer)
+        # workpad
+        self.workpad = wx.TextCtrl(self,style=wx.TE_MULTILINE)
+        self.workpad.SetFont(self.font)
+        self.workpad.SetValue('# workpad\n\n\twords')
+        # shell
+        self.shell = wx.TextCtrl(self,style=wx.TE_MULTILINE)
+        self.shell.SetFont(self.font)
+        self.shell.SetValue('# shell')
         # place
-        self.sizer.Add(self.console,4,wx.EXPAND)
-        self.sizer.Add(self.cmdline,1,wx.EXPAND)
+        self.Vsizer.Add(self.workpad,4,wx.EXPAND)
+        self.Vsizer.Add(self.shell,1,wx.EXPAND)
         self.Layout()
 
 def GUI():
