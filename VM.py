c687eefa358f5921db4cd3234e7a45ed09208e8b
Dmitry Ponyatov <dponyatov@gmail.com>
2018-01-08 12:10:21 +0400

    + PAD interpreter queue
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- VM.py
+++ VM.py
@@ -1,8 +1,18 @@
 # Dynamic FORTH/VM
 
+import Queue
+
 from SYM import *
 
 PAD = String('pad') # active wordpad text (will be parsed/executed)
+
+PAD_Q = Queue.Queue()
+def PAD_runner():
+    while True:
+        PAD.val = PAD_Q.get()
+        print PAD
+        PAD_Q.task_done()
+
 D   = Stack('data') # data stack
 W   = Map('words')  # vocabulary (words)
 
